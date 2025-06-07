bcc903375c901ce830bc41d4e0a2762559d3d8c9
SwathiPriya37 <rswathipriya3@gmail.com>
2025-06-07 13:29:27 +0530

    modified the input format

--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -7,8 +7,30 @@ import password_icon from '../Assets/password.png'
 
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className='container'>
+      <div className="heade">
+        <div className="text">Sign Up</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" name="" id="" />
+        </div>
+        <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" name="" id="" />
+        </div>
+        <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" name="" id="" />
+        </div>
+      </div>
+      <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
+      <div className="submit-container">
+        <div className="submit">Sign Up</div>
+        <div className="submit">Login</div>
+      </div>
     </div>
   )
 }
